1edca2587fc8104b5e832f94c72f456b1f00976f
BuildTools <unconfigured@null.spigotmc.org>
2022-05-11 08:40:06 +0200

    Removed files

--- server/src/commands.js
+++ server/src/commands.js
@@ -1,5 +1,6 @@
 const commands = [
   { id: 1000, command: "abc" },
-  { id: 1001, command: "cba" },
+  { id: 1001, command: '"cba"' },
 ];
+
 module.exports = commands;
--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,20 @@
 require("dotenv").config();
 
-const express = require("express");
 const utils = require("./utils.js");
 const tmi = require("tmi.js");
 const mincraft = require("minecraft-server-util");
 
 const { USERNAME, PASSWORD, CHANNELS, MCRCONHOST, MCRCONPORT, MCRCONPASSWORD } =
   process.env;
-const PORT = 8001;
 
-const app = express();
+// console
 const client = new mincraft.RCON();
 
 const connect = async () => {
   await client.connect(MCRCONHOST, parseInt(MCRCONPORT), {
     timeout: 1000 * 5,
   });
+
   await client.login(MCRCONPASSWORD, {
     timeout: 1000 * 5,
   });
@@ -29,6 +28,11 @@ const twitchClient = new tmi.Client({
   },
   channels: [CHANNELS],
 });
+
+twitchClient
+  .connect()
+  .then((x) => console.log("We are connected to chat!"))
+  .catch(console.error);
 
 twitchClient.on("message", async (channel, tags, message, self) => {
   console.log(tags.username, message);
@@ -46,16 +50,4 @@ twitchClient.on("message", async (channel, tags, message, self) => {
   console.log(result);
 
   await client.close();
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  twitchClient
-    .connect()
-    .then((x) => console.log("We are connected to chat!"))
-    .catch(console.error);
 });
